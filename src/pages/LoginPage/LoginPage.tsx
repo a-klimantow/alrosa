27a1f1aea35639b996d9646e98f254b95222beba
Антон Климантов <klimantow@yandex.ru>
2021-07-16 01:19:21 +0300

    createa LoginPage

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,26 @@
+import { Typography, Button, Box } from "@material-ui/core"
+
+import { Field, Logo } from "components"
+
+export const LoginPage = () => (
+  <Box
+    display="grid"
+    justifyContent="center"
+    alignContent="center"
+    minHeight="100vh"
+  >
+    <Logo mb={8} />
+    <Box component="form" display="grid" gap={2}>
+      <Typography component="h1" variant="h6" fontWeight={700}>
+        Авторизация
+      </Typography>
+
+      <Field label="Логин" />
+      <Field label="Пароль" isPassword />
+
+      <Box mt={3} display="grid">
+        <Button variant="contained">Войти</Button>
+      </Box>
+    </Box>
+  </Box>
+)
