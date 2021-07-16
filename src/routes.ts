2163f04e0c5feb42459e79ed2260c4fcbe922c24
Антон Климантов <klimantow@yandex.ru>
2021-07-17 00:50:34 +0300

    create routes

---
+++ src/routes.ts
@@ -0,0 +1,66 @@
+import { RouteProps } from "react-router-dom"
+
+import { IconType } from "components"
+import { NotFoundPage, HomePage, LoginPage } from "pages"
+
+interface RouteItemType extends RouteProps {
+  name: string
+  icon: IconType | null
+}
+
+export const routes: RouteItemType[] = [
+  {
+    name: "домашняя страница",
+    path: "/",
+    icon: "home",
+    component: HomePage,
+    exact: true,
+  },
+  {
+    name: "договора",
+    path: "/contracts/",
+    icon: "contract",
+    component: HomePage,
+  },
+  {
+    path: "/bids/",
+    component: HomePage,
+    name: "заявки",
+    icon: "bid",
+  },
+  {
+    name: "жалобы",
+    path: "/complaints/",
+    icon: "complaint",
+    component: HomePage,
+  },
+  {
+    name: "счета",
+    path: "/bills/",
+    icon: "bill",
+    component: HomePage,
+  },
+  {
+    name: "профиль",
+    path: "/profile/",
+    icon: "profile",
+    component: HomePage,
+  },
+  {
+    name: "поддержка",
+    path: "/support/",
+    icon: "support",
+    component: HomePage,
+  },
+  {
+    name: "выход",
+    path: "/login/",
+    icon: "logout",
+    component: LoginPage,
+  },
+  {
+    name: "404",
+    icon: null,
+    component: NotFoundPage,
+  },
+]
