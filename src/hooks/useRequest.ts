917895eda898244fb03eb3603c43852629642133
Антон <40547042+a-klimantow@users.noreply.github.com>
2021-07-20 14:32:37 +0300

    Update useRequest.ts
    change base url

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,3 +1,4 @@
+
 import superagent from "superagent"
 import { Keys } from "utils"
 
@@ -6,11 +7,16 @@ type Params = {
   method?: "get" | "post"
 }
 
+const baseUrl =
+  process.env["NODE_ENV"] === "development"
+    ? ""
+    : "https://alros.baccasoft.ru/server"
+
 export const useRequest = ({ url, method = "get" }: Params) => {
   const type = localStorage.getItem(Keys.Type)
   const token = localStorage.getItem(Keys.Token)
 
-  return superagent[method](`/api/v1/${url}`)
+  return superagent[method](`${baseUrl}/api/v1/${url}`)
     .type("application/json")
     .set("Authorization", `${type} ${token}`)
 }
