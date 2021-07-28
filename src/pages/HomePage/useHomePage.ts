c1e4cf59618fe5e881d9a83f1389ec14f3349bf6
Антон Климантов <klimantow@yandex.ru>
2021-07-28 04:54:07 +0300

    clear homePage

--- src/pages/HomePage/useHomePage.ts
+++ src/pages/HomePage/useHomePage.ts
@@ -2,20 +2,15 @@ import React from "react"
 import { useLocalObservable } from "mobx-react-lite"
 import { useHistory } from "react-router-dom"
 
-import { useBidStore } from "store"
 import { useRequest } from "hooks"
-import { ErrorType, ContractDataType, ComplaintDataType } from "types"
+import { ErrorType, ContractDataType } from "types"
 
 export const useHomePage = () => {
   const { replace } = useHistory()
   const requestContracts = useRequest({ url: "contract" })
-  const requestBids = useRequest({ url: "bid" })
-  const requestComplaints = useRequest({ url: "complaint" })
-  const bid = useBidStore()
+
   const store = useLocalObservable(() => ({
     contract: { items: [], total: 0 } as ContractDataType,
-    bid,
-    complaint: { items: [], total: 0 } as ComplaintDataType,
 
     getData() {
       requestContracts
@@ -23,24 +18,10 @@ export const useHomePage = () => {
           this.contract = body
         })
         .catch(this.error)
-
-      // requestBids
-      //   .then(({ body }: { body: BidDataType }) => {
-      //     this.bid = body
-      //   })
-      //   .catch(this.error)
-
-      requestComplaints
-        .then(({ body }: { body: ComplaintDataType }) => {
-          this.complaint = body
-        })
-        .catch(this.error)
     },
 
     cancelRequest() {
       requestContracts.abort()
-      requestBids.abort()
-      requestComplaints.abort()
     },
     error(e: ErrorType) {
       e.message !== "Aborted" && replace("/login/")
